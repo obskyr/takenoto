6521c41b09de08e69e50a684b4e15853a02e09cb
obskyr <powpowd@gmail.com>
2015-04-28 23:15:08 +0200

    Working Emojipedia search! :honey_pot:

--- emojipedia.py
+++ emojipedia.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# ...I'm writing an Emojipedia API. What about it.
+# ...I'm writing a kind of Emojipedia API. What about it.
+# Note: the emojipedia servers are incredibly slow most of the time.
+# I can't do much about it!
 
 import requests
+from urllib import quote_plus
 from bs4 import BeautifulSoup
 
 def getSoup(url):
-    return BeautifulSoup(requests.get(url).text)
+
+    return BeautifulSoup(requests.get(url).content)
+
+class SearchResult(object):
+    def __init__(self, article):
+        h1 = article.find("h1").text.strip()
+        self.emoji, self.name = h1.split(" ", 1)
+
+def search(term):
+    url = "http://emojipedia.org/?s="
+    # Thankfully, emojipedia's search does URL-encoded emoji too.
+    url += quote_plus(term.encode('utf-8'))
+
+    soup = getSoup(url)
+    results = []
+    for article in soup('article', class_='hentry'):
+        results.append(SearchResult(article))
+
+    return results
 
 if __name__ == '__main__':
-    # Alternatively soup.find("div", class_="entry-header").find("h1")
-    # for more redundancy / future-proofing.
-    # A regex (or even a split at space, I suppose) could separate the emoji
-    # from the title.
-    print getSoup("http://emojipedia.org/flag-for-sweden/").find("h1") \
-        .text.__repr__()
+    import sys
+    def doIt():
+        try:
+            term = sys.argv[1]
+        except IndexError:
+            print "Please provide a search term as parameter."
+
+        for result in search(term):
+            print result.emoji.encode('unicode-escape').ljust(11) + result.name
+
+    doIt()
